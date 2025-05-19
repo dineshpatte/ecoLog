f159ecf556d84712218f0ce3d2cb1a11135e6467
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-05-19 21:30:08 +0530

    completed backend setup

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import { User } from "../models/user.model";
+import asyncHandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+const verifyJWT = asyncHandler(async (req, res) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(404, "unauthorized token");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(404, "invalid api error");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "invalid access ");
+  }
+});
